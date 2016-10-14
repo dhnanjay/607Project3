ddd2aa88d24622f28a08db50df41019c13542a3e
haobruce <brucehao@Bruces-iMac.local>
2016-10-13 20:47:20 -0700

    Added MySQL setup scripts and R script to access MySQL db.

---
+++ mySqlR.R
@@ -0,0 +1,16 @@
+library(dbConnect)
+
+username = 'data607'
+password = 'project3'
+host = 'data607-project3.ce2dfe0qxt5q.us-west-2.rds.amazonaws.com'
+myDb = dbConnect(MySQL(), user=username, password=password, dbname='dataScienceSkills', host=host)
+
+# exmple of an insert query; enter NULL for skill_id as skill_id is auto incrementing primary key
+insertQry = dbSendQuery(myDb, "INSERT INTO skill_names (skill_id, skill_name) 
+                        VALUES (NULL, 'R'), (NULL, 'Python');")
+fetch(insertQry, n = -1)
+
+# example of a select query
+selectQry = dbSendQuery(myDb, "SELECT * FROM skill_names;")
+results = fetch(selectQry, n = -1)
+results
