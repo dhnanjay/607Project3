8a30be0e5be56f64a3bcc065cf534850ace46697
deejay <dhan980@gmail.com>
2016-10-15 17:47:44 -0400

    Scrapping data from Indeed (samplecode.R)

--- IndeedScrap.R
+++ IndeedScrap.R
@@ -2,7 +2,7 @@ library(rindeed)
 options(stringsAsFactors = FALSE)
 
 #my_settings <- in_settings()
-
+#bd_ny <- in_scrape(q = "???????????????", l = "??????", p = 2)
 in_setup(country = "US")
 ds_us <- in_scrape("data scientist")
 ds_ny <- in_scrape("data scientist", "New York")
