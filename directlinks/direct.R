15e728fd4737ba8496a17b0c9436e9f679ec6c02
jelikish <joseph.e@gmail.com>
2016-10-17 15:41:38 -0400

    1000 processed

--- directlinks/direct.R
+++ directlinks/direct.R
@@ -1,8 +1,13 @@
 library(boilerpipeR)
 library(RCurl)
+library(stringr)
 
-list = read.csv("c:/data/links.csv")
+#read the file with links
+list = read.csv("c:/data/links.csv", stringsAsFactors = FALSE)
 
+
+
+#parsing each link and saving job text into description column
 for (i in 1:nrow(list))
 {
   content <- getURL(list$Link[i])
@@ -10,5 +15,20 @@ for (i in 1:nrow(list))
   list$description[i] = extract
 }
 
-#write.csv(list, file = "monster.csv")
+#gettong rid of entries with an empty description
+list = subset(list, list$description != "")
 
+
+
+#extracting job title, city, state from url and adding them as columns
+
+for (i in 1:nrow(list))
+{
+  r_extract=str_match(list$Link[i], "http://jobview.monster.com/(.*)-job-(.*)-(\\w\\w)-us.*")
+  list$Job_Title[i] = r_extract[2]
+  list$City[i] = r_extract[3]
+  list$State[i] = r_extract[4]
+}
+
+
+#write.csv(list, file = "monster.csv")
